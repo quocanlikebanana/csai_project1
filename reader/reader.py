ff622760d776bc6b0c86ab9815c31619b61c45e2
NguyenPhamPhuXuan <phuxuan.px2003@gmail.com>
2024-04-12 15:03:36 +0700

    reader

---
+++ reader/reader.py
@@ -0,0 +1,44 @@
+from component.point import Point
+from component.polygon import Polygon
+
+#input = InputReader('input.txt')
+#access to input. width, height, start_point, end_point, number_of_polygons, polygons
+class InputReader:
+    def __init__(self, file_path) -> None:
+        f = open(file_path,'r')
+        file_content  = f.read()
+        lines = file_content.split('\n')
+
+        #width and height of map
+        tmp = lines[0].split(',')
+        self.width = tmp[0]
+        self.height = tmp[1]
+
+        #start and end points
+        tmp = lines[1].split(',')
+        self.start_point = Point(tmp[0], tmp[1])
+        self.end_point = Point(tmp[2], tmp[3])
+
+        #number of polygons
+        self.number_of_polygons = int(lines[2])
+
+        #vertex of polygons
+        self.polygons :list[Polygon] = []
+        for i in range(0, self.number_of_polygons):
+            index = i+3
+            tmp = lines[index].split(',')
+            list_point : list[Point] = []
+            j = 0
+            while j<len(tmp):
+                list_point.append(Point(tmp[j], tmp[j+1]))
+                j = j+2
+            self.polygons.append(Polygon(list_point))
+
+    #for testing
+    def output(self):
+        print(self.width, self.height, '\n')
+        print(self.start_point, self.end_point, '\n')
+        print(self.number_of_polygons, '\n')
+        for i in range(self.number_of_polygons):
+            print(self.polygons[i], '\n')
+
